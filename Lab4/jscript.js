d00b3e15daa2c6edad2493e5e0a0d98c4df4968e
barive <43215251+barive@users.noreply.github.com>
2019-01-29 13:00:45 -0600

    Contestar preguntas lab4

---
+++ Lab4/jscript.js
@@ -0,0 +1,8 @@
+/*1: 
+Entrada: un número pedido con un prompt.
+Salida: Una tabla con los números del 1 al número dado con sus cuadrados y cubos. Utiliza document.write para producir la salida */
+
+function tabla(){
+	var numero = prompt("Introduce un número");
+	
+}
